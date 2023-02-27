62b2a0f9e02e8aa2f6afc21a67704264c6365ca1
Bruno Braga <braga.bruno81@gmail.com>
2023-02-26 19:29:13 -0600

    global menu added to MainActivity

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.ui.login.LoginFragment
 import timber.log.Timber
 
@@ -20,5 +24,17 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             }
             Timber.plant(Timber.DebugTree())
         }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return  NavigationUI.onNavDestinationSelected(item!!,
+                findNavController(this, R.id.container))
+                || super.onOptionsItemSelected(item)
     }
 }
